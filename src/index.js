6012e13c81ce6cbca9495888cbdf991a51eba833
Mani sankar <lakshmimanisankar400@gmail.com>
2024-07-13 15:05:26 +0530

    bootstrap added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import App from "./app";
 import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
